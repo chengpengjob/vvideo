bc3f69eeebb36734427757391971a81f3715ddac
chengpeng <chenpenjob@163.com>
2020-05-07 21:58:31 +0800

    前端页面左侧栏测试（Test1,Test2）

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import {postRequest} from "./utils/api";
+import {postKeyValueRequest} from "./utils/api";
+import {putRequest} from "./utils/api";
+import {deleteRequest} from "./utils/api";
+import {getRequest} from "./utils/api";
+
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.postKeyValueRequest = postKeyValueRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.getRequest = getRequest;
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
